d9e2bae6e6c6d7600b7df57f0c373ac3bfd28f7d
Jamal The Atlantean <jamaltheatlantean@gmail.com>
2022-09-23 21:04:11 +0100

    Update helper-hardhat-config.js

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -13,7 +13,7 @@ const networkConfig = {
   },
   5: {
       name: "goerli",
-      subscriptionId: "21184",
+      subscriptionId: "2298",
       gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc", // 30 gwei
       interval: "30",
       entranceFee: "100000000000000000", // 0.1 ETH
